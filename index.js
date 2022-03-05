6752bf62a5f2e765bc96e9278630ca48b2c46bf0
Graham Zemel <69569628+gzemel@users.noreply.github.com>
2022-03-05 01:36:30 -0500

    added commands

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 (require("dotenv")).config();
 const Discord = require('discord.js');
-const client = new Discord.Client({intents: [
-    "GUILDS",
-    "GUILD_MESSAGES",
-    "GUILD_MEMBERS"
-]});
+const client = new Discord.Client({
+    partials: ['MESSAGE'],
+    intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"]
+});
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", msg => {
     if (msg.content === "hey") {
-    msg.reply("hi there");
+        msg.reply("hi there")
+    } else if (msg.content === "u good bro") {
+        msg.channel.send("nah")
+    } else if (msg.content === "rtn is epic") {
+        msg.react("❤️")
     }
-});
+    else if (msg.content === "gimme mod") {
+        msg.member.roles.add("873405376345813024");
+        }
+})
 
 client.login(process.env.DISCORD_TOKEN);
