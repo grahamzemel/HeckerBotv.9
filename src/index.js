8fb9fb8b4b1441c2dfedac9156e7875b12b8d571
Graham Zemel <69569628+gzemel@users.noreply.github.com>
2022-03-05 00:33:00 -0500

    commit 2

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
- });
+    console.log(`Logged in as ${client.user.tag}!`);
+});
 
 client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
- }
- });
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+});
 
 client.login(process.env.DISCORD_TOKEN);
