eb95dd10b53d2f3c958d3d61016ecac2601b4ff2
Graham Zemel <69569628+gzemel@users.noreply.github.com>
2022-03-05 16:21:58 -0500

    Added currencies

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "help",
     async execute(Discord, client, message, args) {
-        message.channel.send("énchanté, me llamo heckerbot. Some commands include \n -Bored \n -Clear");
+        message.channel.send("énchanté, me llamo heckerbot. Some commands include \n -Bored \n -Clear \n -Ban \n -Kick \n \n CURRENCY CMDS \n -Balance \n -Inventory \n -Transfer \n -Buy \n -Shop \n -Leaderboard");
     },
 };
   
--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const path = require('path');
 
 const prefix = "-";
 
+const { Users, CurrencyShop } = require('./dbObjects');
+const { Op } = require('sequelize');
+const currency = new Discord.Collection();
 
 client.commands = new Discord.Collection();
 const files = fs.readdirSync(path.join(__dirname, "commands"));
@@ -21,42 +24,98 @@ for (const file of files) {
 }
 
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+client.once('ready', async () => {
+	const storedBalances = await Users.findAll();
+	storedBalances.forEach(b => currency.set(b.user_id, b));
+	console.log(`Logged in as ${client.user.tag}!`);
 });
+
 
 client.on("messageCreate", async function (message) {
     if (message.author.bot) return
+    currency.add(message.author.id, 1);
     if (message.content.startsWith(prefix)) {
         const commandBody = message.content.slice(prefix.length);
         const args = commandBody.split(' ');
         let command = client.commands.get(args[0]);
         client.commands.get(command.name).execute(Discord, client, message, args);
     }
-    // if (msg.content === "$inspire") {
-    //     getQuote().then(quote => msg.channel.send(quote))
-    // }
-    // if (msg.content === "hey") {
-    //     msg.reply("hi there")
-    // } else if (msg.content === "u good bro") {
-    //     msg.channel.send("nah")
-    // } else if (msg.content === "gzemel is epic") {
-    //     msg.react("❤️")
-    // }
-    // else if (msg.content === "gimme mod") {
-    //     msg.reply("Applying mod role...")
-    //     msg.member.roles.add("873405376345813024");
-    // }
 
+    if (message.content === '-balance') {
+		const target = message.mentions.users.first() || message.author;
+		return message.channel.send(`${target.tag} has ${currency.getBalance(target.id)}💰`);
+	} else if (message.content  === '-inventory') {
+		const target = message.mentions.users.first() || message.author;
+		const user = await Users.findOne({ where: { user_id: target.id } });
+		const items = await user.getItems();
 
+		if (!items.length) return message.channel.send(`${target.tag} has nothing!`);
+		return message.channel.send(`${target.tag} currently has ${items.map(t => `${t.amount} ${t.item.name}`).join(', ')}`);
+	} else if (message.content  === '-transfer') {
+		const currentAmount = currency.getBalance(message.author.id);
+		const transferAmount = commandArgs.split(/ +/).find(arg => !/<@!?\d+>/.test(arg));
+		const transferTarget = message.mentions.users.first();
+
+		if (!transferAmount || isNaN(transferAmount)) return message.channel.send(`Sorry ${message.author}, that's an invalid amount`);
+		if (transferAmount > currentAmount) return message.channel.send(`Sorry ${message.author} you don't have that much.`);
+		if (transferAmount <= 0) return message.channel.send(`Please enter an amount greater than zero, ${message.author}`);
+
+		currency.add(message.author.id, -transferAmount);
+		currency.add(transferTarget.id, transferAmount);
+
+		return message.channel.send(`Successfully transferred ${transferAmount}💰 to ${transferTarget.tag}. Your current balance is ${currency.getBalance(message.author.id)}💰`);
+	} else if (message.content  === '-buy') {
+		const item = await CurrencyShop.findOne({ where: { name: { [Op.like]: commandArgs } } });
+		if (!item) return message.channel.send('That item doesn\'t exist.');
+		if (item.cost > currency.getBalance(message.author.id)) {
+			return message.channel.send(`You don't have enough currency, ${message.author}`);
+		}
+
+		const user = await Users.findOne({ where: { user_id: message.author.id } });
+		currency.add(message.author.id, -item.cost);
+		await user.addItem(item);
+
+		message.channel.send(`You've bought a ${item.name}`);
+	} else if (message.content  === '-shop') {
+		const items = await CurrencyShop.findAll();
+		return message.channel.send(items.map(i => `${i.name}: ${i.cost}💰`).join('\n'), { code: true });
+	} else if (message.content  === '-leaderboard') {
+		return message.channel.send(
+			currency.sort((a, b) => b.balance - a.balance)
+				.filter(user => client.users.cache.has(user.user_id))
+				.first(10)
+				.map((user, position) => `(${position + 1}) ${(client.users.cache.get(user.user_id).tag)}: ${user.balance}💰`)
+				.join('\n'),
+			{ code: true },
+		);
+	}
+    
 })
-// function getQuote() {
-//     return fetch("https://zenquotes.io/api/random")
-//         .then(res => {
-//             return res.json()
-//         })
-//         .then(data => {
-//             return data[0]["q"] + " -" + data[0]["a"]
-//         })
-// }
+
+
+Reflect.defineProperty(currency, 'add', {
+	/* eslint-disable-next-line func-name-matching */
+	value: async function add(id, amount) {
+		const user = currency.get(id);
+		if (user) {
+			user.balance += Number(amount);
+			return user.save();
+		}
+		const newUser = await Users.create({ user_id: id, balance: amount });
+		currency.set(id, newUser);
+		return newUser;
+	},
+});
+
+Reflect.defineProperty(currency, 'getBalance', {
+	/* eslint-disable-next-line func-name-matching */
+	value: function getBalance(id) {
+		const user = currency.get(id);
+		return user ? user.balance : 0;
+	},
+});
+
+
+
+
 client.login(process.env.DISCORD_TOKEN);
