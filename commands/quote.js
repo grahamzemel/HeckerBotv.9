204b2830c339bf2e427da0749bd7603b3a9a236d
Graham Zemel <69569628+gzemel@users.noreply.github.com>
2022-03-06 12:48:07 -0500

    added quote api

--- commands/quote.js
+++ commands/quote.js
@@ -1,6 +1,11 @@
+const axios = require('axios');
 module.exports = {
     name: "quote",
     async execute(Discord, client, message, args) {
-        
+        axios.get("https://quoteapiforbot.herokuapp.com/").then(function (response) {
+            message.channel.send(`"${response.data}"`);
+        }).catch(function (error) {
+            message.channel.send(err);
+        });
     },
 };
