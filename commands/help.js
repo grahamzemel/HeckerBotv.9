84481106bcd792d8a252a8568d2eca4281c86d8e
Graham Zemel <69569628+gzemel@users.noreply.github.com>
2022-03-06 22:53:00 -0500

    updated sllots

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "help",
     async execute(Discord, client, message, args) {
-        message.channel.send("énchanté, me llamo heckerbot. Some commands include \n -Bored \n -Clear \n -Ban \n -Kick \n -Quote \n -Blackjack \n \nMONEY COMMANDS \n -balance \n -pay \n -payfine");
+        message.channel.send("énchanté, me llamo heckerbot. Some commands include \n -Bored \n -Clear \n -Ban \n -Kick \n -Quote \n -Blackjack \n \nMONEY COMMANDS \n -balance \n -pay \n -slots");
     },
 };
   
--- commands/slots.js
+++ commands/slots.js
@@ -6,14 +6,13 @@ module.exports = {
         const coins = playSlots();
         money.updateBal(message.author.id, coins /* Value */).then((i) => { // money.updateBal grabs the (userID, value) value being how much you want to add, and puts it into 'i'.
             message.channel.send(`**You got $`+ coins +`!**\n**New Balance:** ${i.money}`);
+        
         })
     },
 };
 
 function playSlots(){
-    money.updateBal(message.author.id, -250 /* Value */).then((i) => { // money.updateBal grabs the (userID, value) value being how much you want to add, and puts it into 'i'.
-        message.channel.send(`**You spun the slot machine! $-250!**\n**New Balance:** ${i.money}`);
-    })
+   
     const cherry = new SlotSymbol('cherry', {
         display: '🍒',
         points: 10,
